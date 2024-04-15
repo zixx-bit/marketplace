a3a0ba3b1a245f64b81136542317130bc0691dde
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-04-15 12:26:06 +0300

    package install express

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
-const app = require("./app");
-
+const app = require("./app")
 // handling uncaught Exception
-process.on("uncaughtException",(err) =>{
+process.on("uncaughtException",(err) => {
     console.log(`Error: ${err.message}`);
     console.log(`shutting down the server for handling uncaught exception`);
 })
 
 //config
-if (process.env.NODE_ENV !== "PRODUCTION") {
+if (process.env.NODE_ENV !== "PRODUCTION") {     
     require("dotenv").config({
         path:"backend/config/.env"
     })    
@@ -18,13 +17,13 @@ const server = app.listen(process.env.PORT, ()=>{
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
 })
 
-// unhandles promise rejection
-process.on("unhandlesRejection", (err)=>{
+// unhandled promise rejection
+process.on("unhandledRejection", (err)=>{
     console.log(`Shutting down the server for ${err.message}`);
     console.log(`Shutting down the server for unhandled promise rejection`);
      
     server.close(() =>{
-        process.exit(1)
+        process.exit(1);
     })
 
 })
