d55708ef2f9316587bfcba50383329cb70e527a5
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-04-20 18:57:41 +0300

    server

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,23 @@ const app = require("./app");
 process.on("uncaughtException", (err) => {
     console.log(`Error: ${err.message}`);
     console.log(`shutting down the server for handling uncaught exception`);
+
+})
+
+// create server
+const PORT = app.listen(process.env.PORT || 30000);
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
 })
 
 //config
-if (process.env.NODE_ENV !== "PRODUCTION") {     
+if (process.env.NODE_ENV !== "production") {     
     require("dotenv").config({
         path:"backend/config/.env"
     })    
 }
 
-// create server
-const server = app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on http://localhost:${process.env.PORT}`);
-})
+
 
 // unhandled promise rejection
 process.on("unhandledRejection", (err)=>{
