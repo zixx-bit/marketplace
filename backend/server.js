0c0501c441b9e6e9ffcea0caa444e8f1470026c2
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-04-23 12:33:26 +0300

    database url added

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
-import {PORT} from 'config/.env';
-
 const app = require("./app");
-import {PORT} from 'config/.env';
+
 // handling uncaught Exception
 process.on("uncaughtException", (err) => {
     console.log(`Error: ${err.message}`);
@@ -10,26 +8,17 @@ process.on("uncaughtException", (err) => {
 })
 
 // create server
-const PORT = app.listen(process.env.PORT || 30000);
+// const PORT = app.listen(process.env.PORT || 30000);
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
 })
 
-//config
-if (process.env.NODE_ENV !== "production") {     
-    require("dotenv").config({
-        path:"backend/config/.env"
-    })    
-}
-
-
 // unhandled promise rejection
-process.on("unhandledRejection", (err)=>{
+process.on("unhandledRejection", (err)=>{  
     console.log(`Shutting down the server for ${err.message}`);
     console.log(`Shutting down the server for unhandled promise rejection`);
-     
+
     server.close(() =>{
         process.exit(1);
     })
-
 })
