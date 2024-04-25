d30d4c5b97a1b173323506d9cc940211d6953a15
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-04-25 22:16:09 +0300

    uninstalled express fileupload and install multer

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,12 @@ const ErrorHandler = require("./utils/ErrorHandler");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require ("body-parser");
-const fileUpload = require("express-fileupload");
+// const fileUpload = require("express-fileupload");
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(fileUpload({useTempFiles: true}));
+// app.use(fileUpload({useTempFiles: true}));
 
 //config
 if (process.env.NODE_ENV !== "production"){
