a529dd5724bdd85cb39af80ea9db392feec61373
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-04-26 16:30:05 +0300

    created user model

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,1 +1,59 @@
 const mongoose = require ("mongoose");
+const bcrypt = require ("bcrypt");
+const jwt = require ("jsonwebtoken");
+
+const userSchema = new mongoose.Schema ({
+    name:{
+        type: String,
+        required: [true, "Please enter your name!"],
+    },
+    email:{
+        type: String,
+        required:[true, "Please enter your email"],
+    },
+    password:{
+        type: String,
+        required:[true, "Please enter your password"],
+        minLength:[4, "Password should be greater than 4 characters"],
+        select:false,
+    },
+    phoneNumber:{
+        type:Number,
+    },
+    role:{
+        type:String,
+        default:"user",
+    },
+    avatar:{
+        public_id:{
+            type:String,
+            required: true,
+        },
+        url:{
+            type:String,
+            required:true,
+        }
+    },
+    createdAt:{
+        type: Date,
+        default:Date.now(),
+    },
+
+  
+})
+
+// hash password 
+userSchema.pre("save", async function ( next){
+    if (!this.isModified("password")) {
+        next();
+    }
+    this.password = await bcrypt.hash(this.password, 10)
+});
+
+
+// compare password 
+    userSchema.methods.comparePassword = async function(enteredPassword){
+        return await bcrypt.compare(enteredPassword, this.password);
+    };
+    
+module.exports = mongoose.model("User", userSchema);
