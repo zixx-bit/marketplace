4fd6c4e41ce811272853622d6ba5274738d606fd
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-05-01 17:30:16 +0300

    update on user controller preventing adding of duplicate images on the upload folder

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -4,13 +4,24 @@ const User = require("../model/user");
 const { upload } = require("../multer");
 const ErrorHandler = require("../utils/ErrorHandler");
 const router = express.Router();
+const fs = require("fs");
 
 router.post("/create-user", upload.single("file"), async(req, res, next) =>{
     const {name, email, password} = req.body;
     const userEmail = await User.findOne({email});
 
     if (userEmail) {
-        const filename = req.file.filename
+        const filename = req.file.filename;
+        const filePath = `uploads/${filename}`;
+        fs.unlink(filePath, (error) =>{
+            if (error) {
+                console.log(error);
+                res.status(500).json({message: "Error deleteing file!"})
+            }else{
+                res.json({message: "File deleted successfully"})
+            }
+        })
+
         return next(new ErrorHandler("user already exists", 400));
     }
 
@@ -34,7 +45,7 @@ router.post("/create-user", upload.single("file"), async(req, res, next) =>{
     console.log(user)        
         
     } catch (error) {
-        return next(new ErrorHandler("Error creating user", 400));
+        return next(new ErrorHandler(`Error creating user${error}`, 400));
       
     }
     
--- backend/model/user.js
+++ backend/model/user.js
@@ -1,59 +1,89 @@
-const mongoose = require ("mongoose");
-const bcrypt = require ("bcrypt");
-const jwt = require ("jsonwebtoken");
-const crypto = require ("crypto")
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const userSchema = new mongoose.Schema ({
-    name:{
+const userSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: [true, "Please enter your name!"],
+  },
+  email:{
+    type: String,
+    required: [true, "Please enter your email!"],
+  },
+  password:{
+    type: String,
+    required: [true, "Please enter your password"],
+    minLength: [4, "Password should be greater than 4 characters"],
+    select: false,
+  },
+  phoneNumber:{
+    type: Number,
+  },
+  addresses:[
+    {
+      country: {
         type: String,
-        required: [true, "Please enter your name!"],
-    },
-    email:{
+      },
+      city:{
         type: String,
-        required:[true, "Please enter your email address"],
-    },
-    password:{
+      },
+      address1:{
         type: String,
-        required:[true, "Please enter your password"],
-        minLength:[6, "Password should be greater than 6 characters"],
-        select:false,
-    },
-    phoneNumber:{
+      },
+      address2:{
+        type: String,
+      },
+      zipCode:{
         type: Number,
-    },
-    role:{
+      },
+      addressType:{
         type: String,
-        default: "user",
-    },
-    avatar:{
-        public_id:{
-            type:String,
-            required: true,
-        },
-        url:{
-            type: String,
-            required: true,
-        }
-    },
-    createdAt:{
-        type: Date,
-        default: Date.now,
-    },
-  
-});
-
-// hash password 
-userSchema.pre("save", async function ( next){
-    if (!this.isModified("password")) {
-        next();
+      },
     }
-    this.password = await bcrypt.hash(this.password, 10)
+  ],
+  role:{
+    type: String,
+    default: "user",
+  },
+  avatar:{
+    public_id: {
+      type: String,
+      required: false,
+    },
+    url: {
+      type: String,
+      required: false,
+    },
+ },
+ createdAt:{
+  type: Date,
+  default: Date.now,
+ },
+ resetPasswordToken: String,
+ resetPasswordTime: Date,
 });
 
 
-// compare password 
-    userSchema.methods.comparePassword = async function(enteredPassword){
-        return await bcrypt.compare(enteredPassword, this.password);
-    };
-    
+//  Hash password
+userSchema.pre("save", async function (next){
+  if(!this.isModified("password")){
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// jwt token
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id}, process.env.JWT_SECRET_KEY,{
+    expiresIn: process.env.JWT_EXPIRES,
+  });
+};
+
+// compare password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 module.exports = mongoose.model("User", userSchema);
