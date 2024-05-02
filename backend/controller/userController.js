cc13025a1997350904e4486a36f496fe96379775
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-05-02 11:32:41 +0300

    update user controller

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -54,7 +54,7 @@ router.post("/create-user", upload.single("file"), async(req, res, next) =>{
     const createActivationToken = (user) =>{
         return jwt.sign(user, process.env.ACTIVATION_SECRET, {
             expiresIn: "5m",
-        })
+        });
     }
 });
 
