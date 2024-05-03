16496cd167ef544ca28bb9619cdb50dda91b40ca
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-05-03 23:21:36 +0300

    created and updated sendMail file

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -9,5 +9,12 @@
             user: process.env.SMPT_MAIL,
             pass: process.env.SMPT_PASSWORD,
         },
-    })
+    });
+
+    const mailOptions = {
+        from: process.env.SMPT_MAIL,
+        to: options.email,
+        subject:options.subject,
+        text: options.message,
+    }
  }
